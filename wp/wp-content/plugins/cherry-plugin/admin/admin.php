1bd74e6fbf1f9d94565a14fa0bd7313c8458496a
agentom <ryan.soufi@gmail.com>
2016-11-14 14:48:23 +0100

    maj serveur

--- wp/wp-content/plugins/cherry-plugin/admin/admin.php
+++
@@ -1,6 +0,0 @@
-<?php
-	//includ js and css files
-	include_once (CHERRY_PLUGIN_DIR . 'admin/include-style-script.php');
-	
-	//Shortcodes tinyMCE includes
-	include_once (CHERRY_PLUGIN_DIR . 'admin/shortcodes/tinymce_shortcodes.php');
