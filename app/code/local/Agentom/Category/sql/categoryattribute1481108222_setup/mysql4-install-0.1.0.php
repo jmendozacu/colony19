961053eeb01e1591ae7b1574e66fab5dcb100d16
agentom <ryan.soufi@gmail.com>
2016-12-07 11:59:51 +0100

    category attributes

---
+++ app/code/local/Agentom/Category/sql/categoryattribute1481108222_setup/mysql4-install-0.1.0.php
@@ -0,0 +1,29 @@
+<?php
+$installer = $this;
+$installer->startSetup();
+
+
+$installer->addAttribute("catalog_category", "logo_menu",  array(
+    "type"     => "varchar",
+    "backend"  => "catalog/category_attribute_backend_image",
+    "frontend" => "",
+    "label"    => "Logo du menu",
+    "input"    => "image",
+    "class"    => "",
+    "source"   => "",
+    "global"   => Mage_Catalog_Model_Resource_Eav_Attribute::SCOPE_STORE,
+    "visible"  => true,
+    "required" => false,
+    "user_defined"  => false,
+    "default" => "",
+    "searchable" => false,
+    "filterable" => false,
+    "comparable" => false,
+	
+    "visible_on_front"  => true,
+    "unique"     => false,
+    "note"       => ""
+
+	));
+$installer->endSetup();
+	 
