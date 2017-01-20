ab96977fb19dad7fc6963a8c28aab9aad723ea91
agentom <ryan.soufi@gmail.com>
2017-01-20 09:16:32 +0100

    module video part1

--- app/code/local/Agentom/Video/controllers/Catalog/CategoryController.php
+++ app/code/local/Agentom/Video/controllers/Catalog/CategoryController.php
@@ -2,10 +2,25 @@
 require_once "Mage/Catalog/controllers/CategoryController.php";  
 class Agentom_Video_Catalog_CategoryController extends Mage_Catalog_CategoryController{
 
-    public function postDispatch()
+    protected function _initCatagory()
     {
-        parent::postDispatch();
-        Mage::dispatchEvent('controller_action_postdispatch_adminhtml', array('controller_action' => $this));
+        $category = parent::_initCatagory();
+
+        if($category->getHiddenFromCustomer() && Mage::getSingleton('customer/session')->isLoggedIn()) {
+            $customerData = Mage::getSingleton('customer/session')->getCustomer();
+            $authorizedIds = explode(",",$customerData->getAllowedCategoryIds());
+            if(!in_array($category->getId(),$authorizedIds)){
+                Mage::getSingleton('core/session')->addError($this->__('Accès réservé aux personnes ayant acheté le pack vidéo.'));
+                $this->_redirect('customer/account/login');
+                return false;
+            }
+        }elseif($category->getHiddenFromCustomer()){
+            Mage::getSingleton('core/session')->addError($this->__('Si vous avez accès au pack vidéo, veuillez-vous connecter.'));
+            $this->_redirect('customer/account/login');
+            return false;
+        }
+
+        return $category;
     }
 
 
--- app/code/local/Agentom/Video/controllers/Catalog/ProductController.php
+++ app/code/local/Agentom/Video/controllers/Catalog/ProductController.php
@@ -2,10 +2,9 @@
 require_once "Mage/Catalog/controllers/ProductController.php";  
 class Agentom_Video_Catalog_ProductController extends Mage_Catalog_ProductController{
 
-    public function postDispatch()
-    {
-        parent::postDispatch();
-        Mage::dispatchEvent('controller_action_postdispatch_adminhtml', array('controller_action' => $this));
+    public function viewAction(){
+
+        return parent::viewAction();
     }
 
 
