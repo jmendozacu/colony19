aece5c4a3f327df54514f4af0c420b8f0ff4bcb9
agentom <ryan.soufi@gmail.com>
2017-03-17 16:22:15 +0100

    test

--- app/code/core/Mage/Catalog/controllers/CategoryController.php
+++ app/code/core/Mage/Catalog/controllers/CategoryController.php
@@ -40,7 +40,6 @@ class Mage_Catalog_CategoryController extends Mage_Core_Controller_Front_Action
      */
     protected function _initCatagory()
     {
-        die('test');
         Mage::dispatchEvent('catalog_controller_category_init_before', array('controller_action' => $this));
         $categoryId = (int) $this->getRequest()->getParam('id', false);
         if (!$categoryId) {
