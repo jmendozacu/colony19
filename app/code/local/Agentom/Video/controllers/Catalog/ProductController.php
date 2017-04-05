c0f1842e388ddff97e424caac85cc305add342b9
agentom <ryan.soufi@gmail.com>
2017-04-05 13:17:23 +0200

    maj test

--- app/code/local/Agentom/Video/controllers/Catalog/ProductController.php
+++ app/code/local/Agentom/Video/controllers/Catalog/ProductController.php
@@ -1,60 +1,35 @@
 <?php
 require_once(Mage::getModuleDir('controllers','Mage_Catalog').DS.'ProductController.php');
 class Agentom_Video_Catalog_ProductController extends Mage_Catalog_ProductController{
+    public function viewAction()
+    {
+        // Get initial data from request
+        $categoryId = (int) $this->getRequest()->getParam('category', false);
+        $productId  = (int) $this->getRequest()->getParam('id');
+        $specifyOptions = $this->getRequest()->getParam('options');
 
-//    public function viewAction()
-//    {
-//        // Get initial data from request
-//        $categoryId = (int) $this->getRequest()->getParam('category', false);
-//        $productId  = (int) $this->getRequest()->getParam('id');
-//        $specifyOptions = $this->getRequest()->getParam('options');
-//
-//        // Prepare helper and params
-//        $viewHelper = Mage::helper('catalog/product_view');
-//
-//        $params = new Varien_Object();
-//        $params->setCategoryId($categoryId);
-//        $params->setSpecifyOptions($specifyOptions);
-//
-//        if($categoryId){
-//
-//            $category = Mage::getModel('catalog/category')->load($categoryId);
-//            Mage::log("test product :" .$categoryId,0,"debug_agentom_video.log");
-//
-//            if($category->getHiddenFromCustomer() && Mage::getSingleton('customer/session')->isLoggedIn()) {
-//                $customerData = Mage::getSingleton('customer/session')->getCustomer();
-//                $authorizedIds = explode(",",$customerData->getAllowedCategoryIds());
-//                if(!in_array($category->getId(),$authorizedIds)){
-//                    Mage::getSingleton('core/session')->addError($this->__('Accès réservé aux personnes ayant acheté le pack vidéo.'));
-//                    $this->_redirect('customer/account/login');
-//                    return false;
-//                }
-//            }elseif($category->getHiddenFromCustomer()){
-//
-//                Mage::log("product not logged",0,"debug_agentom_video.log");
-//
-//                Mage::getSingleton('core/session')->addError($this->__('Si vous avez accès au pack vidéo, veuillez-vous connecter.'));
-//                $this->_redirect('customer/account/login');
-//                return false;
-//            }
-//        }
-//
-//
-//        // Render page
-//        try {
-//            $viewHelper->prepareAndRender($productId, $this, $params);
-//        } catch (Exception $e) {
-//            if ($e->getCode() == $viewHelper->ERR_NO_PRODUCT_LOADED) {
-//                if (isset($_GET['store'])  && !$this->getResponse()->isRedirect()) {
-//                    $this->_redirect('');
-//                } elseif (!$this->getResponse()->isRedirect()) {
-//                    $this->_forward('noRoute');
-//                }
-//            } else {
-//                Mage::logException($e);
-//                $this->_forward('noRoute');
-//            }
-//        }
-//    }
+        // Prepare helper and params
+        $viewHelper = Mage::helper('catalog/product_view');
+
+        $params = new Varien_Object();
+        $params->setCategoryId($categoryId);
+        $params->setSpecifyOptions($specifyOptions);
+
+        // Render page
+        try {
+            $viewHelper->prepareAndRender($productId, $this, $params);
+        } catch (Exception $e) {
+            if ($e->getCode() == $viewHelper->ERR_NO_PRODUCT_LOADED) {
+                if (isset($_GET['store'])  && !$this->getResponse()->isRedirect()) {
+                    $this->_redirect('');
+                } elseif (!$this->getResponse()->isRedirect()) {
+                    $this->_forward('noRoute');
+                }
+            } else {
+                Mage::logException($e);
+                $this->_forward('noRoute');
+            }
+        }
+    }
 }
 				
