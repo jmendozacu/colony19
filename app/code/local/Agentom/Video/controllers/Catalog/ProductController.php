03aa1e3927c0d44546caa4e261c594c020d13d18
agentom <ryan.soufi@gmail.com>
2017-04-05 11:20:34 +0200

    maj

--- app/code/local/Agentom/Video/controllers/Catalog/ProductController.php
+++ app/code/local/Agentom/Video/controllers/Catalog/ProductController.php
@@ -4,6 +4,7 @@ class Agentom_Video_Catalog_ProductController extends Mage_Catalog_ProductContro
 
     protected function _initProduct()
     {
+        die('it works');
         $categoryId = (int) $this->getRequest()->getParam('category', false);
         $productId  = (int) $this->getRequest()->getParam('id');
 
