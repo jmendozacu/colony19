f850b9d89a64b9915831946f078b20ebe0e34633
agentom <ryan.soufi@gmail.com>
2017-04-05 13:10:10 +0200

    maj

--- app/code/local/Agentom/Video/controllers/Catalog/ProductController.php
+++ app/code/local/Agentom/Video/controllers/Catalog/ProductController.php
@@ -2,20 +2,20 @@
 require_once(Mage::getModuleDir('controllers','Mage_Catalog').DS.'ProductController.php');
 class Agentom_Video_Catalog_ProductController extends Mage_Catalog_ProductController{
 
-    public function viewAction()
-    {
-        // Get initial data from request
-        $categoryId = (int) $this->getRequest()->getParam('category', false);
-        $productId  = (int) $this->getRequest()->getParam('id');
-        $specifyOptions = $this->getRequest()->getParam('options');
-
-        // Prepare helper and params
-        $viewHelper = Mage::helper('catalog/product_view');
-
-        $params = new Varien_Object();
-        $params->setCategoryId($categoryId);
-        $params->setSpecifyOptions($specifyOptions);
-
+//    public function viewAction()
+//    {
+//        // Get initial data from request
+//        $categoryId = (int) $this->getRequest()->getParam('category', false);
+//        $productId  = (int) $this->getRequest()->getParam('id');
+//        $specifyOptions = $this->getRequest()->getParam('options');
+//
+//        // Prepare helper and params
+//        $viewHelper = Mage::helper('catalog/product_view');
+//
+//        $params = new Varien_Object();
+//        $params->setCategoryId($categoryId);
+//        $params->setSpecifyOptions($specifyOptions);
+//
 //        if($categoryId){
 //
 //            $category = Mage::getModel('catalog/category')->load($categoryId);
@@ -38,23 +38,23 @@ class Agentom_Video_Catalog_ProductController extends Mage_Catalog_ProductContro
 //                return false;
 //            }
 //        }
-
-
-        // Render page
-        try {
-            $viewHelper->prepareAndRender($productId, $this, $params);
-        } catch (Exception $e) {
-            if ($e->getCode() == $viewHelper->ERR_NO_PRODUCT_LOADED) {
-                if (isset($_GET['store'])  && !$this->getResponse()->isRedirect()) {
-                    $this->_redirect('');
-                } elseif (!$this->getResponse()->isRedirect()) {
-                    $this->_forward('noRoute');
-                }
-            } else {
-                Mage::logException($e);
-                $this->_forward('noRoute');
-            }
-        }
-    }
+//
+//
+//        // Render page
+//        try {
+//            $viewHelper->prepareAndRender($productId, $this, $params);
+//        } catch (Exception $e) {
+//            if ($e->getCode() == $viewHelper->ERR_NO_PRODUCT_LOADED) {
+//                if (isset($_GET['store'])  && !$this->getResponse()->isRedirect()) {
+//                    $this->_redirect('');
+//                } elseif (!$this->getResponse()->isRedirect()) {
+//                    $this->_forward('noRoute');
+//                }
+//            } else {
+//                Mage::logException($e);
+//                $this->_forward('noRoute');
+//            }
+//        }
+//    }
 }
 				
