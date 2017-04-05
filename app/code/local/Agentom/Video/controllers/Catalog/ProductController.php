9c3105f476b52aee0293cf1aad10709d75bae89a
agentom <ryan.soufi@gmail.com>
2017-04-05 13:56:53 +0200

    test

--- app/code/local/Agentom/Video/controllers/Catalog/ProductController.php
+++ app/code/local/Agentom/Video/controllers/Catalog/ProductController.php
@@ -1,10 +1,10 @@
 <?php
-require_once 'Mage/Catalog/controllers/ProductController.php';
+require_once(Mage::getModuleDir('controllers','Mage_Catalog').DS.'ProductController.php');
 class Agentom_Video_Catalog_ProductController extends Mage_Catalog_ProductController{
-        public function viewAction()
+
+    public function viewAction()
     {
-        die('toto');
-       return parent::viewAction();
+        return parent::viewAction();
     }
 
 }
