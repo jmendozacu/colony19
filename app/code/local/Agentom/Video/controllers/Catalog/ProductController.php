c098fe96f69effae01f2f63fdf58ada2f951011b
agentom <ryan.soufi@gmail.com>
2017-04-05 11:24:05 +0200

    maj

--- app/code/local/Agentom/Video/controllers/Catalog/ProductController.php
+++ app/code/local/Agentom/Video/controllers/Catalog/ProductController.php
@@ -1,5 +1,5 @@
 <?php
-require_once "Mage/Catalog/controllers/ProductController.php";  
+require_once(Mage::getModuleDir('controllers','Mage_Catalog').DS.'ProductController.php');
 class Agentom_Video_Catalog_ProductController extends Mage_Catalog_ProductController{
 
     protected function _initProduct()
