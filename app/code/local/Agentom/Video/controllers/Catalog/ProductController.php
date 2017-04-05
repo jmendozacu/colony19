264749c4f1f5f8a2adb60ff11e2ad8608b792c40
agentom <ryan.soufi@gmail.com>
2017-04-05 13:21:39 +0200

    maj test

--- app/code/local/Agentom/Video/controllers/Catalog/ProductController.php
+++ app/code/local/Agentom/Video/controllers/Catalog/ProductController.php
@@ -1,5 +1,5 @@
 <?php
-require_once(Mage::getModuleDir('controllers','Mage_Catalog').DS.'ProductController.php');
+require_once 'Mage/Catalog/controllers/ProductController.php';
 class Agentom_Video_Catalog_ProductController extends Mage_Catalog_ProductController{
     /**
      * Current applied design settings
