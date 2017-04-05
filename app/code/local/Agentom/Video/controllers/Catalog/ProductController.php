e501a00af5ebedc80a6f5d53630ba2b25ffb8ca0
agentom <ryan.soufi@gmail.com>
2017-04-05 14:18:13 +0200

    'r

--- app/code/local/Agentom/Video/controllers/Catalog/ProductController.php
+++ app/code/local/Agentom/Video/controllers/Catalog/ProductController.php
@@ -3,43 +3,30 @@ require_once(Mage::getModuleDir('controllers','Mage_Catalog').DS.'ProductControl
 class Agentom_Video_Catalog_ProductController extends Mage_Catalog_ProductController{
 
     /**
-     * Product view action
+     * Initialize requested product object
+     *
+     * @return Mage_Catalog_Model_Product
      */
-    public function viewAction()
+    protected function _initProduct()
     {
-        // Get initial data from request
         $categoryId = (int) $this->getRequest()->getParam('category', false);
         $productId  = (int) $this->getRequest()->getParam('id');
-
 
         $product = Mage::getModel('catalog/product')->load($productId);
         Mage::register('product', $product);
         Mage::register('current_product', $product);
 
-        $specifyOptions = $this->getRequest()->getParam('options');
-
-        // Prepare helper and params
-        $viewHelper = Mage::helper('catalog/product_view');
-
         $params = new Varien_Object();
         $params->setCategoryId($categoryId);
-        $params->setSpecifyOptions($specifyOptions);
 
-        // Render page
-        try {
-            $viewHelper->prepareAndRender($productId, $this, $params);
-        } catch (Exception $e) {
-            if ($e->getCode() == $viewHelper->ERR_NO_PRODUCT_LOADED) {
-                if (isset($_GET['store'])  && !$this->getResponse()->isRedirect()) {
-                    $this->_redirect('');
-                } elseif (!$this->getResponse()->isRedirect()) {
-                    $this->_forward('noRoute');
-                }
-            } else {
-                Mage::logException($e);
-                $this->_forward('noRoute');
-            }
-        }
+        return Mage::helper('catalog/product')->initProduct($productId, $this, $params);
+    }
+    /**
+     * Product view action
+     */
+    public function viewAction()
+    {
+        return parent::viewAction();
     }
 }
 				
