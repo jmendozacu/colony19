334ade83edbc18b6acbc9f8febec9f6f43c68af6
agentom <ryan.soufi@gmail.com>
2017-05-08 17:07:38 +0200

    observer on product attribute for video

--- app/code/local/Agentom/Video/Model/Observer.php
+++ app/code/local/Agentom/Video/Model/Observer.php
@@ -4,9 +4,26 @@ class Agentom_Video_Model_Observer
 
 			public function updateCustomerForAuthorizedCategories(Varien_Event_Observer $observer)
 			{
-				//Mage::dispatchEvent('admin_session_user_login_success', array('user'=>$user));
-				//$user = $observer->getEvent()->getUser();
-				//$user->doSomething();
+                $invoice = $observer->getEvent()->getInvoice();
+                $order = $invoice->getOrder();
+
+                $authorizedId = array();
+                foreach ($order->getAllItems() as $item) {
+                    $product = $item->getProduct();
+                    $product = Mage::getModel('catalog/product')->load($product->getId());
+                    if($product->getGrantAccessToCategory()){
+                        $authorizedId[] = $product->getGrantAccessToCategory();
+                    }
+                }
+
+                if(count($authorizedId)){
+                    $customerId = $order->getCustomerId();
+                    $customer = Mage::getModel('customer/customer')->load($customerId);
+                    $previousIds = explode(",",$customer->getAllowedCategoryIds());
+                    $customer->setAllowedCategoryIds(implode(",",array_merge($previousIds,$authorizedId)));
+                    $customer->save();
+                }
+
 			}
 		
 }
