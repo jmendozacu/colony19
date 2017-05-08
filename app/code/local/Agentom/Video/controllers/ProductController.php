d395c6b50d2f78c05fbb755ceefedad49ee6b6a0
agentom <ryan.soufi@gmail.com>
2017-05-08 10:44:20 +0200

    Adding prodcut redirection

--- app/code/local/Agentom/Video/controllers/ProductController.php
+++ app/code/local/Agentom/Video/controllers/ProductController.php
@@ -6,6 +6,18 @@ class Agentom_Video_ProductController extends Mage_Catalog_ProductController{
      */
     public function viewAction()
     {
+        $productId  = (int) $this->getRequest()->getParam('id');
+        $product = Mage::getModel('catalog/product')->load($productId);
+
+        $cats = $product->getCategoryIds();
+        foreach ($cats as $category_id) {
+            $_cat = Mage::getModel('catalog/category')->load($category_id);
+            if($_cat->getHiddenFromCustomer()){
+                Mage::getSingleton('core/session')->addError($this->__('Accès réservé aux personnes ayant acheté le pack vidéo.'));
+                $this->_redirect('customer/account/login');
+                return false;
+            }
+        }
         return parent::viewAction();
     }
 }
