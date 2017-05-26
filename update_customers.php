9f1c59e86d8761c2dc5b7b0a6a28a5c91dfb8546
agentom <ryan.soufi@gmail.com>
2017-05-26 11:13:18 +0200

    u script

--- update_customers.php
+++ update_customers.php
@@ -11,18 +11,41 @@ try{
     $resource = Mage::getSingleton('core/resource');
     $readConnection = $resource->getConnection('core_read');
     $query ="SELECT so.customer_id FROM mgt_sales_flat_order as so
-    INNER JOIN mgt_sales_flat_order_item as si ON si.order_id = so.entity_id AND (si.product_id = 5360 || si.product_id = 5357)
+    INNER JOIN mgt_sales_flat_order_item as si ON si.order_id = so.entity_id AND si.product_id = 5360
     WHERE so.customer_id IS NOT NULL GROUP BY so.customer_id;";
     $results = $readConnection->fetchAll($query);
     foreach ($results as $customerId){
         echo $customerId . "<br/>";
         $customer = Mage::getModel('customer/customer')->load($customerId);
-        if(is_null($customer->getAllowedCategoryIds()) || empty($customer->getAllowedCategoryIds())){
             echo "Need update <br/>";
+            $customer->setAllowedCategoryIds("251");
+            $customer->save();
+            echo "Done.<br/>";
+    }
+}catch (Exception $e){
+    echo $e->getMessage();
+}
+echo "Finished !";
+
+try{
+    $query ="SELECT so.customer_id FROM mgt_sales_flat_order as so
+    INNER JOIN mgt_sales_flat_order_item as si ON si.order_id = so.entity_id AND si.product_id = 5357
+    WHERE so.customer_id IS NOT NULL GROUP BY so.customer_id;";
+    $results = $readConnection->fetchAll($query);
+    foreach ($results as $customerId){
+        echo $customerId . "<br/>";
+        $customer = Mage::getModel('customer/customer')->load($customerId);
+            echo "Need update <br/>";
+        if($customer->getAllowedCategoryIds()== "251"){
             $customer->setAllowedCategoryIds("250,251");
             $customer->save();
             echo "Done.<br/>";
+        }else{
+            $customer->setAllowedCategoryIds("250");
+            $customer->save();
+            echo "Done.<br/>";
         }
+
     }
 }catch (Exception $e){
     echo $e->getMessage();
