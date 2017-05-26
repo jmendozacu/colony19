bb3ef94527395f2d631eabedc21dab8cf2aed7de
agentom <ryan.soufi@gmail.com>
2017-05-26 09:42:19 +0200

    script

--- update_customers.php
+++ update_customers.php
@@ -7,27 +7,23 @@
  */
 require_once 'app/Mage.php';
 Mage::app();
-
-$product_id = 5357;
-//$product_id = 5360;
-
-$readConnection = $resource->getConnection('core_read');
-$query ="SELECT so.customer_id FROM mgt_sales_flat_order as so
-INNER JOIN mgt_sales_flat_order_item as si ON si.order_id = so.entity_id AND (si.product_id = 5360 || si.product_id = 5357)
-WHERE so.customer_id IS NOT NULL GROUP BY so.customer_id;";
-$results = $readConnection->fetchAll($query);
-foreach ($results as $customerId){
-    echo $customerId . "<br/>";
-    $customer = Mage::getModel('customer/customer')->load($customerId);
-    if(is_null($customer->getAllowedCategoryIds()) || empty($customer->getAllowedCategoryIds())){
-        echo "Need update <br/>";
-        $customer->setAllowedCategoryIds("250,251");
-        try{
+try{
+    $readConnection = $resource->getConnection('core_read');
+    $query ="SELECT so.customer_id FROM mgt_sales_flat_order as so
+    INNER JOIN mgt_sales_flat_order_item as si ON si.order_id = so.entity_id AND (si.product_id = 5360 || si.product_id = 5357)
+    WHERE so.customer_id IS NOT NULL GROUP BY so.customer_id;";
+    $results = $readConnection->fetchAll($query);
+    foreach ($results as $customerId){
+        echo $customerId . "<br/>";
+        $customer = Mage::getModel('customer/customer')->load($customerId);
+        if(is_null($customer->getAllowedCategoryIds()) || empty($customer->getAllowedCategoryIds())){
+            echo "Need update <br/>";
+            $customer->setAllowedCategoryIds("250,251");
             $customer->save();
-        }catch (Exception $e){
-            echo $e->getMessage();
-        }        
-        echo "Done.<br/>";
+            echo "Done.<br/>";
+        }
     }
+}catch (Exception $e){
+    echo $e->getMessage();
 }
 echo "Finished !";
